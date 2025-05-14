a9c0f7c0f242508de234db32328c077fcf8ebcdb
CarloDettori <dettoric07@gmail.com>
2025-05-14 19:00:57 +0200

    can't get datas from context

--- src/context/GlobalContext.jsx
+++ src/context/GlobalContext.jsx
@@ -1,11 +1,30 @@
 import { createContext } from "react";
+import { useState, useEffect } from "react";
 
 const GlobalContext = createContext();
 
 const GlobalProvider = ({ children }) => {
+    const [data, setData] = useState(null)
+
+    async function fechData(url) {
+        try {
+            const response = await fetch(url);
+            const jsonData = await response.json()
+            setData(jsonData)
+        } catch (error) {
+            console.error("Error fetching data:", error)
+        }
+    }
+
+    useEffect(() => fechData(process.env.URL), [])
+        .then(obj => setData(obj), console.log(data))
+        .catch(err => console.error(err))
+        .finally(() => console.log(data))
+
+
 
     return (
-        <GlobalContext.Provider value={{}}>
+        <GlobalContext.Provider value={{ data }}>
             {children}
         </GlobalContext.Provider>
     );
--- src/pages/TaskListPage.jsx
+++ src/pages/TaskListPage.jsx
@@ -1,5 +1,16 @@
+import { useState, useEffect } from "react"
+import { useContext } from "react"
+import { GlobalContext } from "../context/GlobalContext"
+
 export default function TaskListPage() {
+
+    const data = useContext(GlobalContext);
+
+    console.log(data);
+
     return (
+
         <h1>TASK LIST PAGE</h1>
-    )
+
+    );
 }
