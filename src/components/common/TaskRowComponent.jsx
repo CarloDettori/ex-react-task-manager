e8703814b01d26f6d66a47f19ba3a7379c5ae86a
CarloDettori <dettoric07@gmail.com>
2025-05-15 17:09:29 +0200

    milestone 3

---
+++ src/components/common/TaskRowComponent.jsx
@@ -0,0 +1,16 @@
+import { useContext } from "react"
+import { GlobalContext } from "../../context/GlobalContext.jsx"
+import { memo } from "react"
+
+function TaskRowComponent({ tasks }) {
+    console.log(tasks)
+
+    return (
+        <tr>
+            <td style={{ backgroundColor: "rgba(201, 26, 26, 0.17)" }}>{tasks.title}</td>
+            <td style={{ backgroundColor: "rgba(201, 198, 26, 0.17)" }} >{tasks.status}</td>
+            <td style={{ backgroundColor: "rgba(41, 201, 26, 0.17)" }}>{new Date(tasks.createdAt).toLocaleString()}</td>
+        </tr>
+    )
+}
+export default memo(TaskRowComponent)
--- src/pages/TaskListPage.jsx
+++ src/pages/TaskListPage.jsx
@@ -1,18 +1,34 @@
 import { useState, useEffect } from "react"
 import { useContext } from "react"
-import { GlobalContext } from "../context/GlobalContext"
+import { GlobalContext } from "../context/GlobalContext.jsx"
+import TaskRowComponent from "../components/common/TaskRowComponent.jsx";
+import { Button } from "bootstrap";
 
 export default function TaskListPage() {
-
+    const [count, setCount] = useState(0)
     const data = useContext(GlobalContext);
-
-    console.log(data);
+    const tasks = data.data
+    //console.log(tasks);
 
     return (
+        <>
+            <h1>TASK LIST PAGE</h1>
 
-        <h1>TASK LIST PAGE</h1>
+            <table>
+                <tr>
+                    <th>Nome</th>
+                    <th>Stato</th>
+                    <th>Data di Creazione</th>
+                </tr>
+                {
+                    tasks.map((task) => {
+                        return <TaskRowComponent key={task.id} tasks={task} />
+                    })
+                }
+
+            </table>
 
 
-
+        </>
     );
 }
