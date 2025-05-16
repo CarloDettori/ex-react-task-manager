8c1e4ac64e0e460e90bf21c7770ffa706d42931f
CarloDettori <dettoric07@gmail.com>
2025-05-16 18:50:45 +0200

    post sucessfull

--- src/pages/AddTaskPage.jsx
+++ src/pages/AddTaskPage.jsx
@@ -3,18 +3,22 @@ import { useState, useEffect } from "react"
 
 export default function AddTaskPage() {
 
-    const newTask = {
+    const TaskTemplate = {
         title: "",
-        stat: "",
-        dex: "",
+        description: "",
+        status: "",
     }
 
-    const [formInput, setFormInput] = useState(newTask)
+    const [formInput, setFormInput] = useState({
+        title: "",
+        description: "",
+        status: "To do",
+    })
 
     const symbols = `!@#$%^&*()-_=+[]{}|;:'\\",.<>?/~`;
     const titleRef = useRef()
-    const statRef = useRef()
-    const dexRef = useRef()
+    const statusRef = useRef()
+    const descriptionRef = useRef()
 
     const [error, setError] = useState({
         noTitle: false,
@@ -26,7 +30,6 @@ export default function AddTaskPage() {
         const { name, value, type, checked } = e.target;
 
         if (name === "title") {
-            // Blocca solo se il PRIMO carattere è uno spazio
             if (value.length === 1 && value[0] === " ") return;
 
             setError((prevError) => ({
@@ -36,7 +39,6 @@ export default function AddTaskPage() {
             }));
         }
 
-        // Aggiorna sempre lo stato (così puoi scrivere spazi tra le parole)
         setFormInput((prev) => ({
             ...prev,
             [name]: type === 'checkbox' ? checked : value
@@ -65,18 +67,38 @@ export default function AddTaskPage() {
         });
         const noError = anyError.every(error => error === false)
 
+        const url = "http://localhost:3001/tasks";
+        const options = {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json;charset=UTF-8",
+            },
+            body: JSON.stringify(formInput),
+        };
+
         if (noError) {
-            const stat = statRef.current.value.trim()
-            formInput.spec = stat
-            const dex = dexRef.current.value.trim()
-            formInput.exp = dex
+            const status = statusRef.current.value
+            const description = descriptionRef.current.value.trim()
+            const newTask = {
+                ...formInput,
+                status,
+                description,
+            };
             console.log(formInput)
-            setFormInput(newTask)
+            fetch(url, options)
+                .then((response) => response.json())
+                .then((data) => {
+                    console.log(data);
+                });
+            setFormInput(TaskTemplate)
         } else {
             console.error("vlidazione non consentita")
             console.error(error)
         }
     };
+
+
 
     return (<section>
 
@@ -93,7 +115,7 @@ export default function AddTaskPage() {
                     onChange={handleChange}
                     type="text"
                     className="form-control"
-                    id="name"
+                    id="title"
 
 
                 />
@@ -109,25 +131,27 @@ export default function AddTaskPage() {
 
             <p className="form-label">Clicca e scegli uno <strong>Stato</strong></p>
             <select
-                name="spec"
-                id="spec"
-                ref={statRef}
+                name="status"
+                id="status"
+                ref={statusRef}
                 className="form-control selector"
+                value={formInput.status}
+                onChange={handleChange}
             >
-                <option value="" defaultValue>To do</option>
-                <option value="Backend">Doing</option>
-                <option value="FullStack">Done</option>
+                <option value="To do">To do</option>
+                <option value="Doing">Doing</option>
+                <option value="Done">Done</option>
             </select>
 
 
             <div className="my-4">
-                <label htmlFor="dex" className="form-label">Scrivi una <strong>Descrizione</strong></label>
+                <label htmlFor="description" className="form-label">Scrivi una <strong>Descrizione</strong></label>
                 <textarea
-                    ref={dexRef}
-                    name="dex"
+                    ref={descriptionRef}
+                    name="description"
                     type="text"
                     className="form-control"
-                    id="dex"
+                    id="description"
                 />
             </div>
 
@@ -144,7 +168,10 @@ export default function AddTaskPage() {
                             : <button type="submit" className="btn btn-primary mt-5" disabled>Alcuni campi sono sbagliati</button>
                         )
                 }
-                <button type="reset" onClick={() => { setFormInput(newTask); titleRef.current.focus() }} className="btn btn-danger mt-5 ms-auto">Reset</button>
+                <button type="reset" onClick={() => {
+                    setFormInput({ title: "", description: "", status: "", });
+                    titleRef.current.focus()
+                }} className="btn btn-danger mt-5 ms-auto">Reset</button>
             </div>
         </form>
     </section>
